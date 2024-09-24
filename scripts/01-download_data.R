6b17f3f32f318d5f36b74ec979f68ce19696b74f
Angel Xu <anjojoo.xu@mail.utoronto.ca>
2024-09-24 10:15:23 -0400

    Upload files
    Upload all files to match the file format

---
+++ scripts/01-download_data.R
@@ -0,0 +1,25 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Angel Xu
+# Date: 21 September 2024
+# Contact: anjojoo.xu@mail.utoronto.ca
+# Pre-requisites: None
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Download data ####
+# [...ADD CODE HERE TO DOWNLOAD...]
+
+
+
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+         
